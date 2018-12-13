1a292c5f1f313f3111ebba0ab443ad238469b72a
Allen <allendinghamilton@gmail.com>
2018-12-12 23:53:09 -0500

    updates

--- bin/eval.py
+++ bin/eval.py
@@ -14,11 +14,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--prediction-filepath', default=str, required=True)
     parser.add_argument('--ground-truth-filepath', default=str, required=True)
+    parser.add_argument('--prediction-col', default='is_duplicate')
     arg = parser.parse_args()
 
-    y_pred = pd.read_csv(arg.prediction_filepath, usecols=['id', 'is_duplicate']).set_index('id')
+    y_pred = pd.read_csv(
+        arg.prediction_filepath,
+        usecols=['id', arg.prediction_col]).set_index('id')
     y_pred_bin = (y_pred.values > 0.5).astype(np.int)
-    y_true = pd.read_csv(arg.ground_truth_filepath, usecols=['id', 'is_duplicate']).set_index('id')
+    y_true = pd.read_csv(
+        arg.ground_truth_filepath,
+        usecols=['id', 'is_duplicate']).set_index('id')
 
     accuracy = accuracy_score(y_true=y_true, y_pred=y_pred_bin)
     f1score = f1_score(y_true=y_true, y_pred=y_pred_bin)
